a75da4ad20200f208deaa07b9fa5cedb01dc8cd5
Lucchetto <32309609+Lucchetto@users.noreply.github.com>
2020-02-05 17:53:11 +0100

    OmniJaws: make it a product package

--- Android.mk
+++ Android.mk
@@ -20,6 +20,7 @@ include $(CLEAR_VARS)
 LOCAL_JAVA_LIBRARIES := org.apache.http.legacy
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_PACKAGE_NAME := OmniJaws
+LOCAL_PRODUCT_MODULE := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_SDK_VERSION := system_current
 include $(BUILD_PACKAGE)
